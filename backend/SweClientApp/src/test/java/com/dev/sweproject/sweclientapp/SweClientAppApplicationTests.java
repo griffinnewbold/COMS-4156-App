d156e1a39f2e7127299bb18fb36197b438efda3b
Griffin Newbold <griffinnewbold@gmail.com>
2023-11-14 10:57:18 -0500

    initialized spring project

---
+++ backend/SweClientApp/src/test/java/com/dev/sweproject/sweclientapp/SweClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sweproject.sweclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
