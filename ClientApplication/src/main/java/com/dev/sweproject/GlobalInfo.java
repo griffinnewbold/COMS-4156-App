34d553a81f5894b275d96b28b439d59ab8968be6
Griffin Newbold <griffinnewbold@gmail.com>
2023-11-19 00:08:05 -0500

    Added API Endpoints as constants in GlobalInfo.java

--- ClientApplication/src/main/java/com/dev/sweproject/GlobalInfo.java
+++ ClientApplication/src/main/java/com/dev/sweproject/GlobalInfo.java
@@ -3,5 +3,19 @@ package com.dev.sweproject;
 public class GlobalInfo {
 
   public static FirebaseService firebaseDataService;
+  public static final String SERVICE_IP = "http://127.0.0.1:8080";
+  public static final String UPLOAD_URI = "/upload-doc";
+  public static final String SHARE_URI = "/share-doc";
+  public static final String DELETE_URI = "/delete-doc";
+  public static final String SEARCH_URI = "/check-for-doc";
+  public static final String REVISION_URI = "/see-previous-version";
+  public static final String STATS_URI = "/see-document-stats";
+  public static final String DIFFERENCE_URI = "/generate-difference-summary";
+  public static final String DOWNLOAD_URI = "/download-doc";
+
+
+  //IF YOU ARE TESTING FOR CONCURRENCY PLEASE
+  //ALTER THIS BEFORE RUNNING A NEW INSTANCE
+  public static final String NETWORK_ID = "QCO1700369101620";
 
 }
