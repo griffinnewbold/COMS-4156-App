b067f7d83819c9bc4a341ed4c7840de17e20b723
mohsr <mkrizvi98@gmail.com>
2023-11-24 01:15:07 -0500

    Added document management to document page; just need to fill in JS with backend API calls to show real data

--- ClientApplication/src/main/resources/static/js/document.js
+++ ClientApplication/src/main/resources/static/js/document.js
@@ -12,7 +12,55 @@ function refresh_doc()
     // - doc name and version (id: doc-name-label-text)
     // - doc preview (id: preview-data)
     // - doc statistics (id: stats-data)
-    // - versions picker dropdown
+    // - providers / patients dropdown (id: share-select)
+    // - versions picker dropdown (id: version-select)
+}
+
+function download_version()
+{
+    let version_val = $('#version-select').val();
+    if (version_val === null) {
+        alert("You must pick a previous version number to download.");
+        return;
+    }
+    console.log("Downloading version: " + version_val);
+
+    // TODO
+}
+
+function download_latest()
+{
+    // TODO
+}
+
+function upload()
+{
+    const filepicker = document.getElementById('filepicker');
+    if (filepicker.files.length == 0) {
+        alert("You must select a file to upload.");
+        return;
+    }
+
+    // TODO
+}
+
+function share()
+{
+    let share_val = $('#share-select').val();
+    if (share_val === null) {
+        alert("You must pick a patient or provider to share with.");
+        return;
+    }
+    console.log("Sharing with: " + share_val);
+
+    // TODO
+}
+
+function delete_doc()
+{
+    // TODO: delete the document
+
+    goback();
 }
 
 function load_document()
