d15be04e4c2c0c3b35ace27840181a9f68e0de8b
Griffin Newbold <griffinnewbold@gmail.com>
2023-12-06 18:55:47 -0500

    Revert GlobalInfo

--- ClientApplication/src/main/java/com/dev/sweproject/GlobalInfo.java
+++ ClientApplication/src/main/java/com/dev/sweproject/GlobalInfo.java
@@ -83,7 +83,8 @@ public class GlobalInfo {
    * A String representing the specific client using the app.
    * IF YOU ARE TESTING FOR CONCURRENCY PLEASE ALTER THIS BEFORE RUNNING A NEW INSTANCE.
    */
-  public static final String NETWORK_ID = "secondIterationDemo";
+  public static final String NETWORK_ID = "QCO1700369101620";
+  //change to "secondIterationDemo" for demo tomorrow
 
   /**
    * Assigns the global variable for the database.
